47baa638b0dc889b5072f2f3657bd035d497d8bd
emsireli <emsireli@hotmail.com>
2017-06-25 11:57:17 +0100

    Add files via upload

---
+++ SplineInterpolation.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Python 3
+
+This program reads csv files, sorts them and fits a spline curve.
+"""
+
+import csv
+
+with open('data4spl.csv', newline='') as csvFileObj:
+    readerObj = csv.reader(csvFileObj)
+    for row in readerObj:
+        print (row)
